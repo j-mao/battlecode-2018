c73b47802e940841e45c34784b396b728ded1a01
Michael Chen <gendelpiekel@fb.com>
2018-01-12 12:00:01 -0800

    gp-experiment: Made multiple workers build same factory

--- gp-experiment/Player.java
+++ gp-experiment/Player.java
@@ -13,6 +13,8 @@ public class Player {
     private static int dy[] = {1,1,0,-1,-1,-1,0,1,0};
     private static int dx[] = {0,1,1,1,0,-1,-1,-1,0};
 
+    private static int DirCenterIndex = 8; // index 8 corresponds to Direction.Center
+
     static class BfsState {
         public int y, x, startingDir;
 
@@ -34,8 +36,12 @@ public class Player {
     private static boolean hasFriendlyUnit[][];
     // temporary seen array
     private static boolean bfsSeen[][];
+    // which direction you should move to reach each square, according to the bfs
+    private static int bfsDirectionIndexTo[][];
     // to random shuffle the directions
     private static int randDirOrder[];
+    // factories that are currently blueprints
+    private static ArrayList<Unit> factoriesBeingBuilt = new ArrayList<Unit>();
 
     public static void main(String[] args) {
 
@@ -65,10 +71,10 @@ public class Player {
         while (true) {
             System.out.println("Current round: " + gc.round());
 
-            initTurn();
+            VecUnit units = gc.myUnits();
+            initTurn(units);
 
             // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
-            VecUnit units = gc.myUnits();
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
 
@@ -107,6 +113,7 @@ public class Player {
         isPassable = new boolean[height][width];
         hasFriendlyUnit = new boolean[height][width];
         bfsSeen = new boolean[height][width];
+        bfsDirectionIndexTo = new int[height][width];
         for (int y = 0; y < height; y++) for (int x = 0; x < width; x++) {
             System.out.println("checking if there's passable terrain at " + new MapLocation(gc.planet(), x, y).toString());
             //System.out.println("is passable at y = " + y + ", x = " + x + " is " + earthMap.isPassableTerrainAt(new MapLocation(gc.planet(), x, y)));
@@ -126,7 +133,14 @@ public class Player {
         }
     }
 
-    private static void initTurn() {
+    private static void initTurn(VecUnit myUnits) {
+        factoriesBeingBuilt.clear();
+        for (int i = 0; i < myUnits.size(); i++) {
+            Unit unit = myUnits.get(i);
+            if (unit.unitType() == UnitType.Factory && unit.structureIsBuilt() == 0) {
+                factoriesBeingBuilt.add(unit);
+            }
+        }
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 hasFriendlyUnit[y][x] = false;
@@ -163,10 +177,15 @@ public class Player {
         }
     }
 
-    private static Direction bfs(MapLocation from, MapLocation to) {
+    // bfs to all squares
+    // store how close you want to get to your destination in howClose (distance = #moves distance, not Euclidean distance)
+    // stores the direction you should move to be within howClose of each square
+    private static void bfs(MapLocation from, int howClose) {
         int fromY = from.getY(), fromX = from.getX();
-        int toY = to.getY(), toX = to.getX();
-        for (int y = 0; y < height; y++) for (int x = 0; x < width; x++) bfsSeen[y][x] = false;
+        for (int y = 0; y < height; y++) for (int x = 0; x < width; x++) {
+            bfsDirectionIndexTo[y][x] = DirCenterIndex;
+            bfsSeen[y][x] = false;
+        }
         bfsSeen[fromY][fromX] = true;
         Queue<BfsState> queue = new LinkedList<>();
 
@@ -184,8 +203,10 @@ public class Player {
             BfsState cur = queue.peek();
             queue.remove();
 
-            if (cur.y == toY && cur.x == toX) {
-                return directions[cur.startingDir];
+            for (int y = cur.y - howClose; y <= cur.y + howClose; y++) for (int x = cur.x - howClose; x <= cur.x + howClose; x++) {
+                if (0 <= y && y < height && 0 <= x && x < width && bfsDirectionIndexTo[y][x] == DirCenterIndex) {
+                    bfsDirectionIndexTo[y][x] = cur.startingDir;
+                }
             }
 
             shuffleDirOrder();
@@ -198,13 +219,12 @@ public class Player {
                 }
             }
         }
-
-        return Direction.Center;
     }
 
     private static void runWorker(Unit unit) {
         boolean done = false;
 
+        // try to build an adjacent factory blueprint
         if (!done) {
             VecUnit units = gc.senseNearbyUnits(unit.location().mapLocation(), 2);
             for (int i = 0; i < units.size(); i++) {
@@ -217,6 +237,7 @@ public class Player {
             }
         }
 
+        // try to build adjacent factory blueprint (this code is currently unused)
         // looks like hasUnitAtLocation is bugged?
         if (false && !done) {
             // try build blueprint
@@ -233,6 +254,43 @@ public class Player {
             }
         }
 
+        // try to blueprint
+        if (!done) {
+            Direction dir = directions[rand.nextInt(8)];
+            if (factoriesBeingBuilt.isEmpty() && gc.canBlueprint(unit.id(), UnitType.Factory, dir)) {
+                gc.blueprint(unit.id(), UnitType.Factory, dir);
+                MapLocation loc = unit.location().mapLocation().add(dir);
+                hasFriendlyUnit[loc.getY()][loc.getX()] = true;
+                done = true;
+                Unit other = gc.senseUnitAtLocation(loc);
+                System.out.println("The factory I just built is here: " + other.toString());
+                // TODO: continue this
+                // TODO: store ArrayList of current factory blueprints and implement workers moving towards them
+                // TODO: implement worker replication
+                factoriesBeingBuilt.add(gc.senseUnitAtLocation(loc));
+            }
+        }
+
+        // if there is a factory blueprint somewhere, try to move towards it to help build it
+        if (!done && !factoriesBeingBuilt.isEmpty()) {
+            // set done to true so we don't move randomly
+            done = true;
+
+            // bfs to within distance 1 of every square because we want to move within distance 1 of a factory
+            bfs(unit.location().mapLocation(), 1);
+
+            // TODO: change this to closest factory
+            Unit factory = factoriesBeingBuilt.get(0);
+            MapLocation loc = factory.location().mapLocation();
+            Direction dir = directions[bfsDirectionIndexTo[loc.getY()][loc.getX()]];
+            System.out.println("worker moving to factory in dir " + dir.toString());
+
+            if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), dir)) {
+                doMoveRobot(unit, dir);
+            }
+        }
+
+        // otherwise move randomly
         if (!done) {
             Direction dir = directions[rand.nextInt(8)];
 
@@ -240,15 +298,12 @@ public class Player {
             if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), dir)) {
                 doMoveRobot(unit, dir);
             }
-
-            if (gc.canBlueprint(unit.id(), UnitType.Factory, dir)) {
-                gc.blueprint(unit.id(), UnitType.Factory, dir);
-            }
         }
     }
 
     private static void runFactory(Unit unit) {
         if (gc.canProduceRobot(unit.id(), UnitType.Ranger)) {
+            System.out.println("PRODUCING ROBOT!!!");
             gc.produceRobot(unit.id(), UnitType.Ranger);
         }
 
@@ -256,6 +311,9 @@ public class Player {
             Direction unloadDir = directions[j];
             if (gc.canUnload(unit.id(), unloadDir)) {
                 gc.unload(unit.id(), unloadDir);
+                MapLocation loc = unit.location().mapLocation().add(unloadDir);
+                hasFriendlyUnit[loc.getY()][loc.getX()] = true;
+                // TODO: check everywhere else to make sure hasFriendlyUnits[][] is being correctly maintained.
             }
         }
     }
@@ -292,6 +350,8 @@ public class Player {
                     // find closest out of the potential attackable locations
                     // TODO: change this to bfs distance so it's less horrible xd
                     // TODO: once changed to bfs, make sure you don't do too many bfs's and time out
+                    // TODO: actually you can just do one bfs LuL
+                    // TODO: make the bfs result function more smooth over time
                     MapLocation bestLoc = attackLocs.get(0);
                     for (int i = 1; i < attackLocs.size(); i++) {
                         if (moveDistance(unit.location().mapLocation(), attackLocs.get(i)) <
@@ -300,7 +360,8 @@ public class Player {
                         }
                     }
                     System.out.println("doing bfs");
-                    moveDir = bfs(unit.location().mapLocation(), bestLoc);
+                    bfs(unit.location().mapLocation(), 0);
+                    moveDir = directions[bfsDirectionIndexTo[bestLoc.getY()][bestLoc.getX()]];
                     System.out.println("got, from dfs, direction " + moveDir.toString());
                     if (moveDir != Direction.Center && gc.canMove(unit.id(), moveDir)) {
                         doMoveRobot(unit, moveDir);
