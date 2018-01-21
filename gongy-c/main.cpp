5952c8df867fcde2353c47e4c6468f744466afd2
gongy <richardgong0@gmail.com>
2018-01-22 00:49:37 +1100

    clean up flood_fill

--- gongy-c/main.cpp
+++ gongy-c/main.cpp
@@ -8,6 +8,7 @@
 
 #include <utility>
 #include <map>
+#include <set>
 #include <vector>
 #include <queue>
 #include <algorithm>
@@ -60,11 +61,14 @@ static int manhattanDistanceToNearestEnemy[55][55];
 // Warning: constant used, would need to change this if the maps got bigger... but they probably won't get bigger so whatever.
 static const int ManhattanDistanceNotSeen = 499;
 
+
 GameController gc;
 
 static vector<Direction> directions;
 static map<int, int> tendency; // TODO : test unordered map
 static vector<MapLocation> attackLocs;
+
+static set< pair<int,int> > rocketLandingLocations;
 
 static int width, height;
 static bool isPassable[55][55];
@@ -90,6 +94,12 @@ static vector<Unit> rocketsReady;
 // team defs
 static Team myTeam;
 static Team enemyTeam;
+
+// Map type results
+static const int CANT_REACH_ENEMY = 0;
+static const int PARTIALLY_REACH_ENEMY = 1;
+static const int FULLY_REACH_ENEMY = 2;
+static int reach_enemy_result = -1;
 
 // (Currently not used! Just ignore this xd)
 // Map of how long ago an enemy ranger was seen at some square
@@ -131,6 +141,7 @@ static int numEnemiesThatCanAttackSquare[55][55];
 static int numIdleRangers;
 
 static const int MultisourceBfsUnreachableMax = 499;
+static bool can_reach_from_spawn[55][55];
 
 // whether the square is "near" enemy units
 // currently defined as being within distance 72 of an enemy
@@ -201,6 +212,9 @@ static void multisourceBfsAvoidingUnitsAndDanger (vector<SimpleState>& startingL
 static bool doReplicate(Unit& unit);
 static void checkForEnemyUnits (vector<Unit>& allUnits);
 
+static void do_flood_fill(vector<SimpleState>& startingLocs, int resultArr[55][55], bool passableArr[55][55], int label);
+static int can_reach_enemy_on_earth();
+
 static bool bfsTowardsBlueprint(Unit& unit);
 static bool doBuild(Unit& unit);
 static bool doBlueprint(Unit& unit, UnitType toBlueprint);
@@ -250,6 +264,9 @@ int main() {
 
 	init_global_variables();
 	printf("Finished init_global_variables()\n");
+
+	reach_enemy_result = can_reach_enemy_on_earth();
+	printf("reach_enemy_result: %d\n",reach_enemy_result);
 
 	if (myPlanet == Mars) {
 		while (true) {
@@ -329,6 +346,7 @@ int main() {
 				if (!unit.is_on_map()) {
 					continue;
 				}
+
 
 				switch (unit.get_unit_type()) {
 					case Worker:
@@ -515,7 +533,7 @@ static void init_turn (vector<Unit>& myUnits) {
 			good_healer_positions.push_back(make_pair(y, x));
 		}
 	}
-	printf("%d good healer positions\n", SZ(good_healer_positions));
+	// printf("%d good healer positions\n", SZ(good_healer_positions));
 
 	// calculate good positions
 	good_ranger_positions.clear();
@@ -583,6 +601,7 @@ static void start_racing_to_mars () {
 }
 
 static void init_global_variables () {
+
 
 	fo(i, 0, 8) directions.push_back((Direction) i);
 
@@ -1631,6 +1650,70 @@ static void multisourceBfsAvoidingUnitsAndDanger (vector<SimpleState>& startingL
 			}
 		}
 	}
+}
+
+static void do_flood_fill (vector<SimpleState>& startingLocs, bool resultArr[55][55], bool passableArr[55][55]) {
+
+	fo(y, 0, height) fo(x, 0, width) {
+		resultArr[y][x] = false;
+	}
+
+	queue<SimpleState> q;
+	for (SimpleState loc: startingLocs){
+		resultArr[loc.y][loc.x] = true;
+		q.push(loc);
+	}
+
+	while (!q.empty()){
+		SimpleState cur = q.front(); q.pop();
+
+		fo(d, 0, 8) {
+			int ny = cur.y + dy[d];
+			int nx = cur.x + dx[d];
+			if (0 <= ny && ny < height && 0 <= nx && nx < width && passableArr[ny][nx] && !resultArr[ny][nx]) {
+				resultArr[ny][nx] = true;
+
+				q.push(SimpleState(ny,nx));
+			}
+		}
+	}
+}
+
+static int can_reach_enemy_on_earth () {
+
+	//returns either CANT_REACH_ENEMY, PARTIALLY_REACH_ENEMY or FULLY_REACH_ENEMY
+
+	vector<SimpleState> spawn_locs;
+	vector<Unit> initial_units = EarthMap.get_initial_units();
+
+	fo(i, 0, SZ(initial_units)) {
+		Unit& unit = initial_units[i];
+		if (unit.get_team() == myTeam) {
+			MapLocation loc = unit.get_map_location();
+			spawn_locs.push_back(SimpleState(loc.get_y(),loc.get_x()));
+		}
+	}
+
+	do_flood_fill(spawn_locs, can_reach_from_spawn, isPassable);
+
+	bool doReachSomeEnemy = false;
+	bool doMissSomeEnemy = false;
+
+	fo(i, 0, SZ(initial_units)) {
+		Unit& unit = initial_units[i];
+		if (unit.get_team() == enemyTeam){
+			MapLocation loc = unit.get_map_location();
+			if (can_reach_from_spawn[loc.get_y()][loc.get_x()]) {
+				doReachSomeEnemy = true;
+			} else{
+				doMissSomeEnemy = true;
+			}
+		}
+	}
+
+	if (doReachSomeEnemy && !doMissSomeEnemy) return FULLY_REACH_ENEMY;
+	if (doReachSomeEnemy) return PARTIALLY_REACH_ENEMY;
+	return CANT_REACH_ENEMY;
 }
 
 static bool doReplicate(Unit& unit) {
