fc68461061da9652b970dd67ff451bff5001dcd9
Jerry <jerry.w.mao@gmail.com>
2018-01-24 00:25:20 +1100

    Fix spontaneous compilation issues

---
+++ submission/old.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This is the old run.sh
+# Just in case we need to restore
+
+# build the program!
+# note: there will eventually be a separate build step for your bot, but for now it counts against your runtime.
+echo starting script
+
+LIBRARIES="-lutil -ldl -lrt -lpthread -lgcc_s -lc -lm -L/battlecode-c/lib/ -lbattlecode"
+INCLUDES="-I/battlecode-c/include"
+
+echo starting compilation
+
+g++ -std=c++14 -O3 main.cpp -o main $LIBRARIES $INCLUDES -DNOT_IN_DEBUG_MODE
+
+echo compilation finished, starting bot
+
+# run the program!
+./main
--- submission/prepare.sh
+++ submission/prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# The object file is what we submit
-# At this point, all that needs to be done is compile it into an executable
+# We prepare an object file containing our bot
+# NOTE: RUN THIS BEFORE SUBMITTING THE BOT!!!
 
 # we provide this env variable for you
+BC_PLATFORM='LINUX'
 if [ "$BC_PLATFORM" = 'LINUX' ]; then
     LIBRARIES="-lbattlecode-linux -lutil -ldl -lrt -pthread -lgcc_s -lc -lm -L../battlecode/c/lib"
     INCLUDES="-I../battlecode/c/include -I."
@@ -16,7 +17,5 @@ else
 	exit 1
 fi
 
-echo "$ g++ main.o -o main $LIBRARIES -std=c++11"
-g++ main.o -o main $LIBRARIES -std=c++11
-
-./main
+echo "$ g++ -o main.o main.cpp -c -O -g $INCLUDES -std=c++11"
+g++ -o main.o main.cpp -c -O -g $INCLUDES -DNOT_IN_DEBUG_MODE -std=c++11 -fPIC -U_FORTIFY_SOURCE
