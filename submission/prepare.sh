041a02e0089dea7d54e93be22f029378aef69f2e
Jerry <jerry.w.mao@gmail.com>
2018-01-24 11:25:24 +1100

    Add submission

--- submission/prepare.sh
+++ submission/prepare.sh
@@ -4,6 +4,9 @@
 # NOTE: RUN THIS BEFORE SUBMITTING THE BOT!!!
 
 # we provide this env variable for you
+
+rm main.o
+
 BC_PLATFORM='LINUX'
 if [ "$BC_PLATFORM" = 'LINUX' ]; then
     LIBRARIES="-lbattlecode-linux -lutil -ldl -lrt -pthread -lgcc_s -lc -lm -L../battlecode/c/lib"
@@ -19,3 +22,11 @@ fi
 
 echo "$ g++ -o main.o main.cpp -c -O3 -g $INCLUDES -DNDEBUG -std=c++11 -fPIC -U_FORTIFY_SOURCE"
 g++ -o main.o main.cpp -c -O3 -g $INCLUDES -DNDEBUG -std=c++11 -fPIC -U_FORTIFY_SOURCE
+
+result=$?
+
+if [ $result -eq 0 ]; then
+	printf "\033[32mCompilation succeeded. You are ready to submit.\033[0m\n"
+else
+	printf "\033[32mCompilation failed. Please fix this!\033[0m\n"
+fi
